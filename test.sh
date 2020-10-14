c471b3e4ddb52c4b625ecd58e07fb3cb0ee47a76
João Henrique <joao_henriquee@outlook.com>
2020-10-14 08:53:51 -0300

    Altera teste

--- test.sh
+++ test.sh
@@ -9,20 +9,20 @@ rm -rf ./hook-test
 # Default script for running command line in services
 bash ./docker-helper nginx-cli bash -c "cd / && ls -l"
 echo -e "Test 1 finished\n"
-sleep 2
+sleep 1
 
 # Script in the docker-helper.d directory
 bash ./docker-helper nginx bash -c "cd / && ls -l"
 echo -e "Test 2 finished\n"
-sleep 2
+sleep 1
 
 # Script in the docker-helper.yml
-bash ./docker-helper test2 bash -c "cd / && ls -l"
+bash ./docker-helper test1
 echo -e "Test 3 finished\n"
-sleep 2
+sleep 1
 
-# Script in the docker-helper.yml
-bash ./docker-helper test1 -aq
+# Script in the docker-helper.d directory
+bash ./docker-helper test2
 echo -e "Test 4 finished\n"
-sleep 2
+sleep 1
 
